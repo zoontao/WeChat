55e36795fcd8ecbd91563b188876bbd2d2f9c289
Bell <bell@zoontao.com>
2020-02-27 07:23:50 +0800

    Update Client.php

--- src/BasicService/Jssdk/Client.php
+++ src/BasicService/Jssdk/Client.php
@@ -88,7 +88,7 @@ class Client extends BaseClient
      */
     public function getTicket(bool $refresh = false, string $type = 'jsapi'): array
     {
-        $cacheKey = sprintf('easywechat.basic_service.jssdk.ticket.%s.%s', $type, $this->getAppId());
+        $cacheKey = sprintf('wechat.basic_service.jssdk.ticket.%s.%s', $type, $this->getAppId());
 
         if (!$refresh && $this->getCache()->has($cacheKey)) {
             return $this->getCache()->get($cacheKey);
